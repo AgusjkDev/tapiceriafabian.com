9eb188a1d08457eafcae701f5778154cb7cd7674
AgusjkDev <agusjkdev7@gmail.com>
2024-11-14 19:12:00 -0300

    add initial files

---
+++ README.md
@@ -0,0 +1,1 @@
+# tapiceriafabian.com
---
+++ prettier.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig & import("prettier-plugin-tailwindcss").PluginOptions} */
+export default {
+    arrowParens: "avoid",
+    endOfLine: "crlf",
+    tabWidth: 4,
+    importOrder: ["<THIRD_PARTY_MODULES>", "", "^@/(.*)$", "", "^[./]"],
+    importOrderParserPlugins: ["typescript"],
+    tailwindFunctions: ["twMerge"],
+    plugins: [
+        "@ianvs/prettier-plugin-sort-imports",
+        "prettier-plugin-astro",
+        "prettier-plugin-tailwindcss",
+    ],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
+};
