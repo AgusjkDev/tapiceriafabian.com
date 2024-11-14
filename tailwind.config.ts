b4eebe345829f9b815db8ecf4a1f323b576ac968
AgusjkDev <agusjkdev7@gmail.com>
2024-11-14 19:34:55 -0300

    add geist font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
     content: ["./src/**/*.{astro,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Geist", ...fontFamily.sans],
+            },
+        },
     },
     plugins: [],
 } satisfies Config;
