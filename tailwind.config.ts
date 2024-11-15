f86790a71592744ddde44568bc02d62708da2171
AgusjkDev <agusjkdev7@gmail.com>
2024-11-14 22:02:26 -0300

    add hero

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,38 @@ export default {
             fontFamily: {
                 sans: ["Geist", ...fontFamily.sans],
             },
+            fontSize: {
+                title: [
+                    "clamp(24px, 12vw, 64px)",
+                    {
+                        fontWeight: "900",
+                        lineHeight: "1.2",
+                        letterSpacing: "-0.015em",
+                    },
+                ],
+                subtitle: [
+                    "clamp(16px, 9vw, 32px)",
+                    {
+                        fontWeight: "800",
+                        lineHeight: "1.1",
+                    },
+                ],
+            },
+            animation: {
+                "fade-in-down": "fade-in-down 0.5s both",
+            },
+            keyframes: {
+                "fade-in-down": {
+                    "0%": {
+                        opacity: "0",
+                        transform: "translateY(-16px)",
+                    },
+                    "100%": {
+                        opacity: "1",
+                        transform: "translateY(0px)",
+                    },
+                },
+            },
         },
     },
     plugins: [],
